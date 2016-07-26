8e529eca08dd57273aa21d258b836a96a1819712
ibloomltd <edith@ibloom.at>
2016-07-27 01:08:58 +0200

    commented slider in sidebar - customer doesn´t want it anymore (right now :-()

--- sidebar.php
+++ sidebar.php
@@ -17,7 +17,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	<?php wp_reset_query();
 	global $post;
 	$template = get_post_meta($post->ID,'_wp_page_template',true);
-	if ((is_page() || is_single() || is_home()) && $template = 'default') : ?>
-		<?php get_template_part( 'template-parts/sidebar-testimonials' ); ?>
-	<?php endif;?>
+	/* if ((is_page() || is_single() || is_home()) && $template = 'default') : ?>
+		<?php get_template_part( 'template-parts/sidebar-testimonials' ); 
+	 endif; */ ?> 
 </aside><!-- #secondary -->
